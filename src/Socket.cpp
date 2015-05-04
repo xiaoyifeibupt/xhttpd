04d4f31e13e35f20fe0db79c8456c36630538023
xiaoyifeibupt <xiaoyifeibupt@outlook.com>
2015-05-05 07:45:39 +0800

    更

---
+++ src/Socket.cpp
@@ -0,0 +1,145 @@
+#include <netdb.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+#include "Socket.h"
+#include "Log.h"
+
+Address::Address(std::string _host, uint16_t _port) : a_host(_host), a_port(_port) {
+
+}
+
+Socket::Socket() : needToWrite_(false) {
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1) {
+		THROW_SYSTEM_ERROR(); 
+	}
+	int one = 1;
+	int ret = setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &one, sizeof(one));
+	if (ret != 0) {
+		THROW_SYSTEM_ERROR();
+	}
+}
+
+Socket::Socket(Socket&& rhs) : 
+sockfd(std::move(rhs.sockfd)), 
+addr(rhs.addr), 
+host(rhs.host), 
+port(rhs.port)
+{
+	rhs.sockfd = 0;
+}
+
+Socket::~Socket() {
+
+}
+
+void Socket::bind(Address address) {
+	struct hostent *hosten = gethostbyname(address.a_host.c_str());
+
+	if (!hosten) {
+		const char *errMsg = hstrerror(h_errno);
+		throw std::runtime_error(errMsg);
+	}
+
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(address.a_port);
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	int ret = ::bind(sockfd, reinterpret_cast<struct sockaddr*>(&addr),sizeof(addr));
+	if (ret != 0) {
+		THROW_SYSTEM_ERROR();
+	}
+}
+
+void Socket::listen() {
+	int ret = ::listen(sockfd, 10000);
+
+	if (ret != 0) {
+		THROW_SYSTEM_ERROR();
+	}
+}
+
+SocketPtr Socket::accept() {
+	socklen_t size = sizeof(addr);
+	int ret = ::accept(sockfd, reinterpret_cast<struct sockaddr*>(&addr), &size);
+
+	if (ret == -1) {
+		THROW_SYSTEM_ERROR();
+	}
+
+	return std::make_shared<Socket>(ret);
+}
+
+void Socket::makeNonBlocking() {
+	long one = 1;
+
+	int ret = ioctl(sockfd, FIONBIO, &one);
+
+	if (ret == -1) {
+		THROW_SYSTEM_ERROR();
+	}
+}
+
+void Socket::read(Buffer<uint8_t>& buffer) {
+	uint8_t buf[1024];
+	int ret = 0;
+
+	ret = recv(sockfd, buf, sizeof(buf), 0);
+	while (ret > 0) {
+		buffer.append(buf, ret);
+		ret = recv(sockfd, buf, sizeof(buf), 0);
+	}
+}
+
+size_t Socket::write(const uint8_t *buf, size_t size) {
+	writeBuffer.append(buf, size);
+
+	return write();
+}
+
+size_t Socket::write() {
+	int ret = 0;
+
+	ret = ::write(sockfd, writeBuffer.data(), writeBuffer.size());
+	if (ret == -1) {
+		if (errno != EAGAIN && errno != EWOULDBLOCK) {
+			_E("Can't write to socket. Socket will be closed");
+		}
+	} else {
+		writeBuffer.drain(ret);
+	}
+
+	needToWrite = !!writeBuffer.size();
+
+	return writeBuffer.size();
+}
+
+int Socket::native() {
+	return sockfd;
+}
+
+bool Socket::needToWrite() const {
+	return needToWrite;
+}
+
+Socket::Socket(int fd) : sockfd(fd) {
+
+}
+
+
+bool Socket::error() {
+	int optval;
+	socklen_t optlen;
+
+	optlen = sizeof(optval);
+	getsockopt(sockfd, SOL_SOCKET, SO_ERROR, &optval, (socklen_t *)&optlen);
+
+	return optval;
+}
+
+bool operator==(SocketPtr lhs, int rhs) {
+	return lhs->sockfd == rhs;
+}
---
+++ src/Socket.h
@@ -0,0 +1,62 @@
+#ifndef __SOCKET_H__
+#define __SOCKET_H__
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#include <memory>
+#include <string>
+
+#include "Buffer.h"
+
+struct Address {
+	Address(std::string host, uint16_t port);
+	std::string a_host;
+	uint16_t a_port;
+};
+
+class Socket;
+
+typedef std::shared_ptr<Socket> SocketPtr; //指向Socket类的智能指针
+
+class Socket
+{
+public:
+
+	Socket();
+	Socket(int fd);
+	Socket(Socket&&);
+	virtual ~Socket();
+
+	void bind(Address address);
+	void listen();
+	SocketPtr accept();
+	
+	void makeNonBlocking();
+	
+	void read(Buffer<uint8_t>& buffer);
+	
+	
+	size_t write(const uint8_t *buf, size_t size);
+	size_t write();
+	
+	int native();
+	bool error();
+	bool needToWrite() const;
+
+	friend bool operator==(SocketPtr lhs, int rhs);
+
+private:
+	Socket(const Socket&) = delete;
+	Socket& operator=(const Socket&) = delete;
+
+	int sockfd;
+	struct sockaddr_in addr;
+
+	std::string host;
+	uint16_t port;
+	Buffer<uint8_t> writeBuffer;
+	bool needToWrite;
+};
+
+#endif
